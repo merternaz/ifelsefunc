cf200fff7a00c8a82eaa6743f236c318c60fd57a
Mert Ernaz <rnzappdroid@gmail.com>
2023-03-27 22:10:29 +0300

    if

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=1;
+
+            string s=a <20 && a>2 ? "aaa" :a>=20 ? "bbb":"ccc";
+            Console.WriteLine(s);
+
+        }
+    }
+}
